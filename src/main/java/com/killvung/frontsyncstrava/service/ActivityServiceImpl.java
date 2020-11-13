d3d753dc52675a3d39cd96875fd6fc922e474b43
killvung <killvung@gmail.com>
2020-11-13 17:36:05 -0600

    Drafted a fetch for now

--- src/main/java/com/killvung/frontsyncstrava/service/ActivityServiceImpl.java
+++ src/main/java/com/killvung/frontsyncstrava/service/ActivityServiceImpl.java
@@ -1,12 +1,26 @@
 package com.killvung.frontsyncstrava.service;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.reflect.TypeToken;
 import com.killvung.frontsyncstrava.dto.model.ActivityDto;
-import com.killvung.frontsyncstrava.model.Activity;
 import com.killvung.frontsyncstrava.repository.ActivityRepository;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Response;
+import io.swagger.client.ApiClient;
+import io.swagger.client.ApiException;
+import io.swagger.client.ApiResponse;
+import io.swagger.client.JSON;
+import io.swagger.client.api.ActivitiesApi;
+import io.swagger.client.api.AthletesApi;
+import io.swagger.client.model.SummaryActivity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Observable;
 
 @Component
 public class ActivityServiceImpl implements ActivityService {
@@ -19,7 +33,19 @@ public class ActivityServiceImpl implements ActivityService {
 
 	@Override
 	public List<ActivityDto> fetchActivities() {
-		// TODO: Call Strava API endpoint to fetch activities
+		// TODO: Move this to elsewhere
+		ApiClient apiClient = new ApiClient();
+		// TODO: Generate a new access token
+//		apiClient.setAccessToken("");
+		ActivitiesApi api = new ActivitiesApi(apiClient);
+		try {
+			Call call = api.getLoggedInAthleteActivitiesCall(null, null, null, null, null, null);
+			Type localVarReturnType = (new TypeToken<JsonArray>() {}).getType();
+			ApiResponse<JsonArray> a = api.getApiClient().execute(call, localVarReturnType);
+			System.out.println(a.getData().get(0).getAsJsonObject().get("distance"));
+		} catch (ApiException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 }
